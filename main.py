646f63dae952cc69132071231bb0a41143c4fe30
Denise Adati <denisekfa@gmail.com>
2020-06-29 00:53:32 -0300

    imprimindo a saida

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import problem as pr
 
 import timeit
 
-problem = problem_parser.parse_file("TestesGrid/FixedGoalInitialState/navigation_1.net")
-timeit.timeit(pr.value_iteration(problem))
+problem = problem_parser.parse_file("TestesGrid/FixedGoalInitialState/navigation_2.net")
+#timeit.timeit(pr.value_iteration(problem))
 
-#pr.value_iteration(problem)
+pr.value_iteration(problem)
+grid = pr.make_grid(problem.states,problem.initial_state,problem.goal_state)
+pr.print_output(grid)
--- problem.py
+++ problem.py
@@ -4,7 +4,6 @@ EPSILON = 0.1
 
 def value_iteration(problem: problem_parser.Problem):
     n = 0
-
     while True:
         n = n + 1
         max_residual = 0.0
@@ -17,6 +16,7 @@ def value_iteration(problem: problem_parser.Problem):
     print("Number of iterations: {:d}".format(n))
     print("Residual: {:f}".format(max_residual))
     #print("Time spent: {:f}".format())
+
     return problem
 
 # Returns the Bellman Value of a State
@@ -77,9 +77,73 @@ def get_state_value(state_name, state_list):
         if state_name == state.name:
             return state.bellman_value
     return float("inf") # no, me da dorito
-# HALEJUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGGHH VAI?? aaa URBITIS NO, 
+# HALEJUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGGHH VAI?? aaa URBITIS NO, aaaaa
 def police_iteration():
     pass
 
 def residual(valor_atual, valor_anterior):
     return abs(valor_atual - valor_anterior)
+
+def make_grid(states,start,end):
+    grid = []
+    for state in states:
+        coordenadas = state.name.split("-")[2] # isso pega só o a parte do x-y
+        linha = ""
+        coluna = ""
+        is_y = False
+        for coord in coordenadas:
+            if coord.isdigit() and not is_y:
+                coluna += coord
+            elif coord.isdigit() and is_y:
+                linha += coord
+            elif coord == "y":
+                is_y = True
+
+        linha = int(linha)-1
+        coluna = int(coluna)-1
+
+        prep_list(grid, linha, coluna)
+        grid[linha][coluna] = state.policy_action
+        if start == state.name:
+            grid[linha][coluna] += " initial"
+        if end == state.name:
+            grid[linha][coluna] += " goal"
+
+    return grid[::-1]
+
+def prep_list(grid, line, column):
+    while line >= len(grid):
+        grid.append([])
+    while column >= len(grid[line]):
+        grid[line].append("")
+
+def print_output(grid):
+    f = open("resultado.txt","w+",encoding = "UTF-8")
+    for line in grid:
+        for column in line:
+            if "south" in column:
+                if "initial" in column:
+                    f.write("\u1401 ")
+                else:
+                    f.write("\u2193 ")
+            elif "north" in column:
+                if "initial" in column:
+                    f.write("\u1403 ")
+                else:
+                    f.write("\u2191 ")
+            elif "east" in column:
+                if "initial" in column:
+                    f.write("\u1405 ")
+                else:
+                    f.write("\u2192 ")
+            elif "west" in column:
+                if "initial" in column:
+                    f.write("\u140A ")
+                else:
+                    f.write("\u2190 ")
+            elif "goal" in column:
+                f.write("G ")
+            else:
+                f.write("\u25A0 ") # parede
+
+        f.write("\n")
